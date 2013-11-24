e63306ba0cbbad05c989f2ee9dcbbed3bae3d6cb
Adrian Gaudebert <adrian@gaudebert.fr>
2013-11-24 14:33:55 +0100

    Added all screens and a Screen system.

--- js/game.js
+++ js/game.js
@@ -8,11 +8,22 @@ require.config({
 require(['level', 'scene', 'screen'], function (Level, Scene, Screen) {
     "use strict";
 
-    var lvl = new Level(2);
-    lvl.load(function () {
-        // The level is loaded and has its map and riddles.
-        // Create a new Scene object, and pass it the level, then start it.
-        var scene = new Scene(lvl);
-        scene.init();
+    var splash = new Screen('splash');
+    splash.display();
+
+    var lvl1 = new Level(2);
+    lvl1.load(function () {
+        var title = new Screen('title');
+        splash.hide();
+        title.display(function () {
+            var credits = new Screen('credits');
+            credits.display(function () {
+                var tutorial = new Screen('tutorial');
+                tutorial.display(function () {
+                    var scene = new Scene(lvl1);
+                    scene.init();
+                });
+            });
+        });
     });
 });
--- js/screen.js
+++ js/screen.js
@@ -5,17 +5,65 @@ define(['lib/jquery'], function () {
         this.container = $('#screen');
     };
 
-    Screen.prototype.display = function () {
+    Screen.prototype.display = function (next) {
         if (this.name == 'title') {
-            this.displayTitle();
+            this.displayTitle(next);
         }
+        else if (this.name == 'splash') {
+            this.displaySplash(next);
+        }
+        else if (this.name == 'credits') {
+            this.displayCredits(next);
+        }
+        else if (this.name == 'tutorial') {
+            this.displayTutorial(next);
+        }
+
+        this.container.show();
     };
 
-    Screen.prototype.displayTitle = function () {
+    Screen.prototype.displayTitle = function (next) {
+        var self = this;
+
+        $('<img>', { src: 'img/screen/title-1.png' }).appendTo(this.container);
+        var titleImg = $('<img>', { src: 'img/screen/title-2.png', class: 'blink' }).appendTo(this.container);
+
+        titleImg.click(function (e) {
+            e.preventDefault();
+            self.hide();
+            next();
+        });
+    };
+
+    Screen.prototype.displaySplash = function (next) {
+        $('<img>', { src: 'img/screen/splash.png' }).appendTo(this.container);
+    };
+
+    Screen.prototype.displayCredits = function (next) {
+        var self = this;
+
+        var img = $('<img>', { src: 'img/screen/credits.png' }).appendTo(this.container);
+        img.click(function (e) {
+            e.preventDefault();
+            self.hide();
+            next();
+        });
+    };
+
+    Screen.prototype.displayTutorial = function (next) {
+        var self = this;
+
+        var img = $('<img>', { src: 'img/screen/tutorial.png' }).appendTo(this.container);
+        img.click(function (e) {
+            e.preventDefault();
+            self.hide();
+            next();
+        });
     };
 
     Screen.prototype.hide = function () {
         this.container.empty();
+        this.container.hide();
     };
 
     return Screen;
