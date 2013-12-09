eff2b0c3ef85ce75bfb9d1be6302162c3e5c6577
rauduon <superchinois26@gmail.com>
2013-12-09 17:23:51 +0100

    optimisation : riddle selection :)

--- js/riddles.js
+++ js/riddles.js
@@ -1,23 +1,18 @@
 define(['tools'], function (tools) {
     var Riddles = function (riddles) {
         this.riddles = riddles;
-
-        this.givenRiddles = [];
+        this.count = 0;
         this.numberOfRiddles = riddles.length;
     };
 
     Riddles.prototype.getRandomRiddle = function () {
-        if (this.givenRiddles.length == this.numberOfRiddles) {
-            // All Riddles where given, start from scratch again.
-            this.givenRiddles = [];
-        }
 
-        var index = tools.getRandomInt(0, this.numberOfRiddles - 1);
-        while (this.givenRiddles.indexOf(index) != -1) {
-            index = tools.getRandomInt(0, this.numberOfRiddles - 1);
+        if (this.count == this.numberOfRiddles) {
+            // re-shuffle all riddles if we displayed them all
+            tools.shuffleArray(this.riddles);
+            this.count = 0;
         }
-        this.givenRiddles.push(index);
-        return this.riddles[index];
+        return this.riddles[this.count++];
     }
 
     return Riddles;
